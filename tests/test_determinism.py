cb9f767d8f2c36c3630f65cbf2d4642bd231e4bf
Marie Laurent <mlaurent@tellae.fr>
2026-09-18 16:38:27 +0200

    fix:  correct test

--- tests/test_determinism.py
+++ tests/test_determinism.py
@@ -120,7 +120,7 @@ def _test_determinism(index, data_path, tmpdir):
     manager.check(
         "ile_de_france_activities.csv",
         "{}/ile_de_france_activities.csv".format(output_path),
-        "7bb6fa6cb6b65977270ee01d84bcd3f6")
+        "f4cff28ba079add7d403b320eb6778e9")
 
     manager.check(
         "ile_de_france_trips.csv",
@@ -140,7 +140,7 @@ def _test_determinism(index, data_path, tmpdir):
     manager.check(
         "ile_de_france_activities.gpkg",
         "{}/ile_de_france_activities.gpkg".format(output_path),
-        "a3b57243f566b3ec076811c6cd6c6caa")
+        "ca8066414a7607762ab86d5e5237c7c8")
 
     manager.check(
         "ile_de_france_commutes.gpkg",
@@ -155,7 +155,7 @@ def _test_determinism(index, data_path, tmpdir):
     manager.check(
         "ile_de_france_trips.gpkg",
         "{}/ile_de_france_trips.gpkg".format(output_path),
-        "c81947c1a299af93e241025544f83516")
+        "a5eb97504a869952a3084dd6f1e752f6")
 
     manager.finish()
 
